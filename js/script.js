bb7a32aba3a6567c160eb8ff36bb7b2722c05030
Lucas Fortes <fortes.barman@gmail.com>
2025-02-23 22:10:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.getElementById('discord-login').addEventListener('click', () => {
-  const clientId = 'SEU_CLIENT_ID_DISCORD';
+  const clientId = '1336360272520286269';
   const redirectUri = 'https://api.ofc-rede.workers.dev/callback'; // URL do Cloudflare Worker
   const scope = 'identify';
   window.location.href = `https://discord.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scope}`;
