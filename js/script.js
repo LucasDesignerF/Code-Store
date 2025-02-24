d8fe6c702c5794492cc5b4a815ca7ad8595247e3
Lucas Fortes <fortes.barman@gmail.com>
2025-02-23 22:37:42 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ document.getElementById('discord-login').addEventListener('click', () => {
   const scope = 'identify';
   window.location.href = `https://discord.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scope}`;
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
